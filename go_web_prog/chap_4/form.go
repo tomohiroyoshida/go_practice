0b5dd13c2f9ab8754803a1f68facf1983a7aab3f
tomo <tomohiroyoshida0511@gmail.com>
2021-07-12 23:35:36 +0900

    finish chap_4

---
+++ go_web_prog/chap_4/form.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func process(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fmt.Fprintln(w, "(1)", r.FormValue("hello"))
+	fmt.Fprintln(w, "(2)", r.PostFormValue("hello"))
+	fmt.Fprintln(w, "(3)", r.PostForm)
+	fmt.Fprintln(w, "(4)", r.MultipartForm)
+}
+
+func main() {
+	server := http.Server{
+		Addr: "127.0.0.1:8080",
+	}
+	http.HandleFunc("/process", process)
+	server.ListenAndServe()
+}
